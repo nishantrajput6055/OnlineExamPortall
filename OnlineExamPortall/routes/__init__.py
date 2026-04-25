e59f32f2c7858c97d001fd4a20c25635a2e86b7c
Amandeep <amandeepjol@gmail.com>
2026-04-25 19:04:55 +0530

    converted submodule to normal folder

---
+++ OnlineExamPortall/routes/__init__.py
@@ -0,0 +1,4 @@
+"""
+Routes package initialization
+"""
+from . import auth, admin, student, exam
---
+++ OnlineExamPortall/utils/__init__.py
@@ -0,0 +1,6 @@
+"""
+Utilities package initialization
+"""
+from . import database, decorators, helpers
+
+__all__ = ['database', 'decorators', 'helpers']
